64a75d66602f88e1bfeb4fc418aa8a377e83de75
sandhya484 <sandhyayadav26122003@gmail.com>
2026-05-06 20:20:40 +0530

    Exp3(SJF).cpp

---
+++ Exp3(SJF).cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, bt[10], wt[10], tat[10];
+
+    cout<<"Enter number of processes: ";
+    cin>>n;
+
+    cout<<"Enter burst times:\n";
+    for(int i=0;i<n;i++)
+        cin>>bt[i];
+
+    // Sorting (simple bubble sort)
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(bt[i] > bt[j]) {
+                int temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+
+    for(int i=1;i<n;i++)
+        wt[i] = wt[i-1] + bt[i-1];
+
+    for(int i=0;i<n;i++)
+        tat[i] = wt[i] + bt[i];
+
+    cout<<"\nBT\tWT\tTAT\n";
+    for(int i=0;i<n;i++)
+        cout<<bt[i]<<"\t"<<wt[i]<<"\t"<<tat[i]<<endl;
+
+    return 0;
+}
