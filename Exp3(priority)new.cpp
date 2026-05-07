9bbeb467e2b5bd0d76882d1886a24b926997dbff
sandhya484 <sandhyayadav26122003@gmail.com>
2026-05-07 05:39:13 +0530

    Exp3(priority)new.cpp

---
+++ Exp3(priority)new.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n, bt[10], pr[10], wt[10], tat[10];
+
+    cout<<"Enter number of processes: ";
+    cin>>n;
+
+    for(int i=0;i<n;i++) {
+        cout<<"Enter BT and Priority: ";
+        cin>>bt[i]>>pr[i];
+    }
+
+    // sorting by priority
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+
+            if(pr[i] > pr[j]) {
+
+                swap(pr[i], pr[j]);
+                swap(bt[i], bt[j]);
+            }
+        }
+    }
+
+    wt[0]=0;
+
+    for(int i=1;i<n;i++) {
+        wt[i]=wt[i-1]+bt[i-1];
+    }
+
+    for(int i=0;i<n;i++) {
+        tat[i]=wt[i]+bt[i];
+    }
+
+    cout<<"\nBT\tPR\tWT\tTAT\n";
+
+    for(int i=0;i<n;i++) {
+        cout<<bt[i]<<"\t"<<pr[i]<<"\t"<<wt[i]
+        <<"\t"<<tat[i]<<endl;
+    }
+
+    return 0;
+}
